ec581ee224d21663fa44f7e2f6546ae66c5857d3
MuhamedMagdy97 <Mohamed.Magdy1997@outlook.com>
2024-07-12 02:42:10 +0300

    Add some style and Using "useNavigate"

--- src/components/Customers/Customers.jsx
+++ src/components/Customers/Customers.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import Table from '../Table/Table'
+import React from "react";
+import Table from "../Table/Table";
+import { useNavigate } from "react-router-dom";
 
 function Customers() {
-
-
+  const navigate = useNavigate();
+  const handleAddCustomer = () => {
+    navigate("/AddingCustomer"); 
+  };
   return (
-   <div className="container Customers">
-     <div className="row">
+    <div className="container Customers">
+      <div className="row">
         <div className="col-md-12">
-        <h3 className='text-center text-center text-main my-3 h1'>Customers</h3>
-        <div className="tbl">
-      <h2 className="h4 text-sub p-3">Customer Transactions Table</h2>
-      <Table />
-      </div>
+          <h3 className="text-center text-center text-main my-3 h1">
+            Customers
+          </h3>
+          <div className="tbl">
+            <h2 className="h4 text-sub p-1">Customer Transactions Table</h2>
+          <div className="Add-cust text-center m-2">
+          <button className="btn btn-primary" onClick={handleAddCustomer}>
+                Add customer
+              </button>
+          </div>
+            <Table />
+          </div>
         </div>
-     </div>
-   </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Customers
+export default Customers;
