60f608112e94cf2d423fd2a9ddc50286c4414f24
MuhamedMagdy97 <Mohamed.Magdy1997@outlook.com>
2024-07-12 01:29:42 +0300

    create table component

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+function Table() {
+  return (
+    <>
+      <div className="Table">
+        <h2 className="h4 text-sub p-3">Customer Transactions Table</h2>
+        <div className="row ">
+            <div className="col-md-8 ">
+            <table class="table text-center ">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Name</th>
+              <th scope="col">Amout</th>
+              <th scope="col">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>youln</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>youln</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>youln</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>youln</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>youln</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>youln</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>youln</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>youln</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>youln</td>
+              <td>200</td>
+              <td>2/4/2022</td>
+            </tr>
+          </tbody>
+        </table>
+            </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Table;
