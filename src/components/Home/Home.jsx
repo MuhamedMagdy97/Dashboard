08dce3cc4da80855ab4fced9654a14ca23f14f7a
MuhamedMagdy97 <Mohamed.Magdy1997@outlook.com>
2024-07-12 01:28:53 +0300

    Create Home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Table from '../Table/Table'
+
+function Home() {
+  return <>
+  <div className='container'>
+  <h1 className='text-center text-main'>Dashboard</h1>
+  
+  <Table />
+  </div>
+  </>
+}
+
+export default Home
