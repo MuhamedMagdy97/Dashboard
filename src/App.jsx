fc1a2bf751bb22c67800b50688d965252ba15661
MuhamedMagdy97 <Mohamed.Magdy1997@outlook.com>
2024-07-12 02:28:50 +0300

    create and add to route AddTrans

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./components/Layout/Layout";
 import Customers from "./components/Customers/Customers";
 import Transactions from "./components/Transactions/Transactions";
 import AddingCustomer from "./components/AddingCustomer/AddingCustomer";
+import AddTrans from "./components/AddTrans/AddTrans";
 
 function App() {
   let routers = createBrowserRouter([
@@ -27,6 +28,10 @@ function App() {
         {
           path: "/AddingCustomer",
           element: <AddingCustomer />,
+        },
+        {
+          path: "/AddTrans",
+          element: <AddTrans />,
         },
       ],
     },
---
+++ src/components/AddTrans/AddTrans.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddTrans() {
+  return (
+    <div>AddTrans</div>
+  )
+}
+
+export default AddTrans
