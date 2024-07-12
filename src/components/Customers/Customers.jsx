91b363609b3f89fa490d9e8ce22742e8f06a14b1
MuhamedMagdy97 <Mohamed.Magdy1997@outlook.com>
2024-07-12 03:24:37 +0300

    refactor the structure of Customers.jsx

--- src/components/Customers/Customers.jsx
+++ src/components/Customers/Customers.jsx
@@ -7,20 +7,37 @@ function Customers() {
   const handleAddCustomer = () => {
     navigate("/AddingCustomer"); 
   };
+
   return (
     <div className="container Customers">
       <div className="row">
         <div className="col-md-12">
-          <h3 className="text-center text-center text-main my-3 h1">
+          <h3 className="text-center text-main my-3 h1">
             Customers
           </h3>
           <div className="tbl">
             <h2 className="h4 text-sub p-1">Customer Transactions Table</h2>
-          <div className="Add-cust text-center m-2">
-          <button className="btn btn-primary" onClick={handleAddCustomer}>
-                Add customer
-              </button>
-          </div>
+           
+            <div className="row mb-3">
+              <div className="col-md-6">
+                <form className="d-flex" role="search">
+                  <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search by Name or Amount"
+                    aria-label="Search"
+                  />
+                  <button className="btn btn-outline-success" type="submit">
+                    Search 
+                  </button>
+                </form>
+              </div>
+              <div className="col-md-2 text-end">
+                <button className="btn btn-primary w-100" onClick={handleAddCustomer}>
+                  Add customer
+                </button>
+              </div>
+            </div>
             <Table />
           </div>
         </div>
