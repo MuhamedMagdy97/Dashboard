fa105c7940c45fc8eb3c52ef7f6cc36f0ea127be
MuhamedMagdy97 <Mohamed.Magdy1997@outlook.com>
2024-07-12 16:04:13 +0300

    update responsive for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,64 +4,53 @@ import { Link, useLocation } from "react-router-dom";
 function Navbar() {
   const location = useLocation();
   const [selected, setSelected] = useState(location.pathname);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    setSelected(location.pathname); // Update selected state when location changes
+    setSelected(location.pathname); 
   }, [location.pathname]);
 
+  const toggleNavbar = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <nav className="navbar-expand-lg">
-      <div className="container-nav flex-column">
-        <Link className="navbar-brand text-main h1" to="/">
-          Admin
-        </Link>
-        <div className="navbar">
-          <ul className="nav flex-column">
+    <nav className={`navbar ${isOpen ? "open" : ""}`}>
+      <div className="container nav-main">
+        <div className="Logo ">
+          <Link className="navbar-brand text-main h1" to="/">
+            Admin
+          </Link>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleNavbar} 
+          aria-controls="navbarNav"
+          aria-expanded={isOpen}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`navbar-collapse mt-5 ${isOpen ? "show" : ""}`} id="navbarNav">
+          <ul className="navbar-nav flex-column">
             <li className={`nav-item ${selected === "/" ? "act" : ""}`}>
-              <Link
-                className="nav-link"
-                to="/"
-                onClick={() => setSelected("/")}
-              >
+              <Link className="nav-link" to="/" onClick={() => setSelected("/")}>
                 Dashboard
               </Link>
             </li>
-            <li
-              className={`nav-item ${
-                selected.startsWith("/customers") ? "act" : ""
-              }`}
-            >
-              <Link
-                className="nav-link"
-                to="/customers"
-                onClick={() => setSelected("/customers")}
-              >
+            <li className={`nav-item ${selected.startsWith("/customers") ? "act" : ""}`}>
+              <Link className="nav-link" to="/customers" onClick={() => setSelected("/customers")}>
                 Customers
               </Link>
             </li>
-            <li
-              className={`nav-item ${
-                selected.startsWith("/transactions") ? "act" : ""
-              }`}
-            >
-              <Link
-                className="nav-link"
-                to="/transactions"
-                onClick={() => setSelected("/transactions")}
-              >
+            <li className={`nav-item ${selected.startsWith("/transactions") ? "act" : ""}`}>
+              <Link className="nav-link" to="/transactions" onClick={() => setSelected("/transactions")}>
                 Transactions
               </Link>
             </li>
-            <li
-              className={`nav-item ${
-                selected.startsWith("/Charts") ? "act" : ""
-              }`}
-            >
-              <Link
-                className="nav-link"
-                to="/Charts"
-                onClick={() => setSelected("/Charts")}
-              >
+            <li className={`nav-item ${selected.startsWith("/charts") ? "act" : ""}`}>
+              <Link className="nav-link" to="/charts" onClick={() => setSelected("/charts")}>
                 Charts
               </Link>
             </li>
