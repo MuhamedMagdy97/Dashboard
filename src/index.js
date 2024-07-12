6dc791d9a0b9465e2f4e714b22fd5ed2e681f1d5
MuhamedMagdy97 <Mohamed.Magdy1997@outlook.com>
2024-07-12 03:10:51 +0300

    config react query

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+  </>
+);
 
 reportWebVitals();
